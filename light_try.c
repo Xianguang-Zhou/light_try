1457e6b50e0dbdfbd0d4027e174fd0e3fafbb38b
Xianguang-Zhou <xianguang.zhou@outlook.com>
2019-12-19 09:35:17 +0800

    Change license.

--- light_try.c
+++ light_try.c
@@ -1,11 +1,20 @@
 /*
- * Copyright (c) 2018, Xianguang Zhou <xianguang.zhou@outlook.com>. All rights reserved.
+ * Copyright (c) 2018, 2019, Xianguang Zhou <xianguang.zhou@outlook.com>. All
+ * rights reserved.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include <stdlib.h>
 
 #ifdef _MSC_VER
@@ -84,4 +93,3 @@ void ___lt_throw() {
 #endif
 	}
 }
-
